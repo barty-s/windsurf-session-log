5b3602a52ee8ca589712c026a01a303c8dcaedee
barty-s <117319197+barty-s@users.noreply.github.com>
2024-01-11 18:20:21 +0100

    Add redirect code for logged in user to their sessions page

--- session_log/urls.py
+++ session_log/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("mysessions/", views.my_sessions, name="my_sessions"),
 ]
--- session_log/views.py
+++ session_log/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render 
+from django.views import generic
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Session
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -6,3 +11,14 @@ def index(request):
     Render the index.html template
     """
     return render(request, "index.html")
+
+
+@login_required
+def my_sessions(request):
+    """
+    Render the my_sessions.html template with user's logged training sessions
+    """
+    user = request.user
+    user_sessions = Session.objects.filter(author=user)
+
+    return render(request, "my_sessions.html", {"user_session":user_sessions},)
