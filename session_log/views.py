b874614d17d5c69e44cc2b6809608bdea3b953d8
barty-s <117319197+barty-s@users.noreply.github.com>
2024-01-11 14:18:29 +0100

    Add basic template structure for authentication pages

--- session_log/views.py
+++ session_log/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render 
 
 
+def index(request):
+    """
+    Render the index.html template
+    """
+    return render(request, "index.html")
--- windsurfer/urls.py
+++ windsurfer/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', include('session_log.urls'), name='session_log_urls'),
 ]
