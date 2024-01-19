c83d09bed67f86e984fc8b2c3f126ce22173f7d0
barty-s <117319197+barty-s@users.noreply.github.com>
2024-01-19 18:48:48 +0100

    Add CSS styling to common elements across all pages

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 - https://docs.djangoproject.com/en/5.0/topics/class-based-views/generic-display/
 - https://stackoverflow.com/questions/4941974/django-how-to-set-datefield-to-only-accept-today-future-dates
 - https://stackoverflow.com/questions/61031491/can-i-limit-the-dates-possibilities-using-django-widget-dateinput-ex-between-2
+- https://unsplash.com/ - for open-source images
--- windsurfer/settings.py
+++ windsurfer/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
+    'active_link',
     'cloudinary',
     'crispy_forms',
     'session_log',
@@ -146,7 +147,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
