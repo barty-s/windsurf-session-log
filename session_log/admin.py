d817a9363eb21039c5f63137f3a3bbb70935ac2a
barty-s <117319197+barty-s@users.noreply.github.com>
2024-02-01 11:50:20 +0100

    Add list/filter to Django admin

--- session_log/admin.py
+++ session_log/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
 from .models import Session
+from django.contrib.sites.models import Site
+from allauth.socialaccount.models import SocialAccount, SocialApp, SocialToken
 
-admin.site.register(Session)
+
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ('author', 'title', 'date')
+    list_filter = ('author', 'title', 'date')
+
+
+# The following lines are added to remove the default django admin apps
+admin.site.unregister(Site)
+admin.site.unregister(SocialAccount)
+admin.site.unregister(SocialApp)
+admin.site.unregister(SocialToken)
